731603daf1666940d5fa67481e434d1e41a2e9de
Mateus Pontes <mateushspontes@gmail.com>
2023-08-20 22:42:14 -0300

    Adicionando uma lista de topicos, para simular banco de dados e adicionado metodo buscarPorId

--- forum/src/main/kotlin/br/com/alura/forum/model/Usuario.kt
+++ forum/src/main/kotlin/br/com/alura/forum/model/Usuario.kt
@@ -1,2 +1,7 @@
-package br.com.alura.forum.model 
+package br.com.alura.forum.model
 
+data class Usuario(
+    val id: Long? = null,
+    val nome: String,
+    val email: String
+)
--- forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,4 +1,73 @@
 package br.com.alura.forum.service
 
-class TopicoService {
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class TopicoService(private var topicos: List<Topico>) {        //Criado lista para simular um banco de dados
+
+    init {
+        val topico1 = Topico(
+            id = 1,
+            titulo = "Primeiro Topicos",
+            mensagem = "Primeira mensagem",
+            curso = Curso(
+                id = 2,
+                nome = "Formacao Kotlin",
+                categoria = "BackEnd"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Gabriel Dragone",
+                email = "gabriel@gmail.com"
+            )
+        )
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Primeiro Topicos",
+            mensagem = "Primeira mensagem",
+            curso = Curso(
+                id = 2,
+                nome = "Formacao Kotlin",
+                categoria = "BackEnd"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Gabriel Dragone",
+                email = "gabriel@gmail.com"
+            )
+        )
+
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Primeiro Topicos",
+            mensagem = "Primeira mensagem",
+            curso = Curso(
+                id = 2,
+                nome = "Formacao Kotlin",
+                categoria = "BackEnd"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Gabriel Dragone",
+                email = "gabriel@gmail.com"
+            )
+        )
+
+        topicos = Arrays.asList(topico1, topico2, topico3)
+
+    }
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream().filter({ t ->            //Criado metodo buscar por ID
+            t.id == id
+        }).findFirst().get()
+    }
 }
