3a7c180cbd5292cef7f3794ddbfd4a214beb8df4
Mateus Pontes <mateushspontes@gmail.com>
2023-09-12 21:57:44 -0300

    Realizando mapeando das entidades JPA e config application.yml

--- forum/src/main/kotlin/br/com/alura/forum/model/Topico.kt
+++ forum/src/main/kotlin/br/com/alura/forum/model/Topico.kt
@@ -1,14 +1,21 @@
 package br.com.alura.forum.model
 
+import jakarta.persistence.*
 import java.time.LocalDateTime
 
+@Entity // Sinalizando que e uma tabela no banco de dados JPA
 data class Topico(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     val titulo: String,
     val mensagem: String,
     val dataCriacao: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne  //Um curso pode ter varios topicos, mas um topico pertence ao um unico curso
     val curso: Curso,
+    @ManyToOne
     val autor: Usuario,
+    @Enumerated(value = EnumType.STRING)   //para salvar enums no banco
     val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
+    @OneToMany(mappedBy = "topico") // Um topico tem varias respostas
     val respostas: List<Resposta> = ArrayList()
 )
